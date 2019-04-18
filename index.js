282f11b84413cad85ce187f2b4721b220c8ee62e
Lai Weng Han <jlwhoo7@gmail.com>
2019-04-18 10:22:06 +0800

    fix random_food issue

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 
 const foodsFile = "public/foods.json";
 const eatenFile = "public/eaten.json";
+const randomFoodFile = "public/random_food.json";
 
 const getFoodsJson = async () => {
   const foodsJson = await fs.readFile(foodsFile, "utf-8");
@@ -22,25 +23,37 @@ const getEatenJson = async () => {
   return JSON.parse(eatenJson);
 };
 
-async function remindFood(seed) {
-  const _foods = await getFoodsJson();
-  const eaten = await getEatenJson();
-  const foods = _foods.filter(f => eaten.indexOf(f) < 0);
+const sayAction = text => {
+  return new Promise((resolve, reject) => {
+    say.speak(text, "Samantha", 1.0, resolve);
+  });
+};
 
+async function reportLunch(seed, food) {
   let counter = 1;
-  const rng = seedrandom(seed);
-  const i = Math.floor(rng() * foods.length);
-  const food = foods[i];
 
-  say.speak(`Today bitcoin is at ${seed}`);
+  sayAction(`Today bitcoin is at ${seed}`);
 
   const clear = setInterval(() => {
     counter++;
     if (counter > 3) {
       clearInterval(clear);
     }
-    say.speak(`Today we eat ${food}`);
+    sayAction(`Today we eat ${food}`);
   }, 5000);
+}
+
+async function remindFood(seed, cb) {
+  const _foods = await getFoodsJson();
+  const eaten = await getEatenJson();
+  const foods = _foods.filter(f => eaten.indexOf(f) < 0);
+
+  const rng = seedrandom(seed);
+  const i = Math.floor(rng() * foods.length);
+  const food = foods[i];
+  const data = { food, price: seed };
+
+  return data;
 }
 
 const bitcoinPrice = () => {
@@ -119,6 +132,29 @@ app.delete("/api/eaten", async (req, res) => {
   } catch (err) {
     res.status(403).json({ success: false, message: err.message });
   }
+});
+
+// create tts
+app.post("/api/tts", (req, res) => {
+  try {
+    const { message } = req.body;
+    if (typeof message !== "string") {
+      throw new Error("message must be string");
+    }
+    say.speak(message);
+    res.status(200).json({ success: true });
+  } catch (err) {
+    res.status(403).json({ success: false, message: err.message });
+  }
+});
+
+app.get("/api/random_food", async (req, res) => {
+  const data = await bitcoinPrice();
+  const seed = data.data["bitcoin"]["usd"];
+  const d = await remindFood(seed);
+  await fs.writeFile(randomFoodFile, JSON.stringify(d));
+  await reportLunch(d.price, d.food);
+  res.status(200).json({ success: true, data: d });
 });
 
 app.listen("3000", () => {
